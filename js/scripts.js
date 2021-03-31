39bb9c791b2908113b3d7fada4120c68205f46d6
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-03-31 02:03:01 -0700

    show the project after covering material

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,21 @@
-$(document).ready(function() {
-  $("button#green").click(function() {
+$(document).ready(function () {
+  $("button#green").click(function () {
     $("body").removeClass();
     $("body").addClass("green-background");
   });
 
-  $("button#yellow").click(function() {
+  $("button#yellow").click(function () {
     $("body").removeClass();
     $("body").addClass("yellow-background");
   });
 
-  $("button#red").click(function() {
+  $("button#red").click(function () {
     $("body").removeClass();
     $("body").addClass("red-background");
   });
 
-  $("button#grey").click(function() {
+  $("button#grey-toggle").click(function () {
     $("body").removeClass();
-    $("body").addClass("grey-background");
-    //$("body").fadeIn("grey-background");
+    $("body").addClass("grey-background");//how to make this toggle?
   });
 });
